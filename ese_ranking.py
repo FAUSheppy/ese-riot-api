26ade15c79f9da648cbb6f21875e328071cc3ff0
Yannik Schmidt <sheppy@atlantishq.de>
2020-10-06 15:36:57 +0200

    retry if ratelimit

--- ese_ranking.py
+++ ese_ranking.py
@@ -6,6 +6,7 @@ import time
 import argparse
 import os
 from jinja2 import Environment, FileSystemLoader
+from requests.exceptions import HTTPError
 
 WATCHER = None
 REGION  = "euw1"
@@ -30,6 +31,45 @@ def divisionToNumber(division):
                     "IV"  : 100 }
     return divisionmap[division]
 
+def api(iterable):
+
+    players = []
+    missing = []
+    count = 0
+
+    for l in iterable:
+        count += 1
+        if count > 9:
+            time.sleep(1)
+        try:
+            pTmp = WATCHER.summoner.by_name(REGION, l.strip())
+        except HTTPError:
+            missing += [l]
+            print("Backing off for 2minutes because of Rate Limit")
+            time.sleep(120)
+
+        if not pTmp:
+            continue
+
+        try:
+            pInfo = WATCHER.league.by_summoner(REGION, pTmp["id"])
+        except HTTPError:
+            missing += [l]
+            print("Backing off for 2minutes because of Rate Limit")
+            time.sleep(120)
+
+        for queue in pInfo:
+            if queue["queueType"] != "RANKED_SOLO_5x5":
+                continue
+            queue["sorter"] = tierToNumber(queue["tier"]) \
+                                + divisionToNumber(queue["rank"]) \
+                                + int(queue["leaguePoints"])
+            queue.pop("summonerId")
+            queue.pop("leagueId")
+            players += [dict(queue)]
+
+    return (missing, players)
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='ESE Overlord')
@@ -40,28 +80,15 @@ if __name__ == "__main__":
         key = f.read().strip()
         WATCHER = riotwatcher.LolWatcher(key)
 
-    players = []
-    count = 0
+    playersAll = []
     with open("players.txt") as f:
-        for l in f:
-            count += 1
-            if count > 9:
-                time.sleep(1)
-            pTmp = WATCHER.summoner.by_name(REGION, l.strip())
-            if not pTmp:
-                continue
-            pInfo = WATCHER.league.by_summoner(REGION, pTmp["id"])
-            for queue in pInfo:
-                if queue["queueType"] != "RANKED_SOLO_5x5":
-                    continue
-                queue["sorter"] = tierToNumber(queue["tier"]) \
-                                    + divisionToNumber(queue["rank"]) \
-                                    + int(queue["leaguePoints"])
-                queue.pop("summonerId")
-                queue.pop("leagueId")
-                players += [dict(queue)]
+        missing, players = api(f)
+        missing, playersMissed = api(missing)
+        playersAll = playersAll + playersMissed 
+        if missing:
+            print("Still missing some players:", missing)
     
-    players = sorted(players, key=lambda p: p["sorter"], reverse=True)
+    players = sorted(playersAll, key=lambda p: p["sorter"], reverse=True)
     with open(args.target_file, "w") as f:
         f.write(json.dumps(players, indent=4))
 
